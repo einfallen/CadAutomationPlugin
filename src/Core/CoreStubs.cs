578be6fcf7e9038113dc62a66890724941cad05c
CAD Developer <dev@cadautomation.com>
2026-03-12 12:14:04 +0800

    Fix CoreStubs.cs - remove duplicate class definitions
    Error: The namespace 'CadAutomationPlugin.Core.BOM' already contains a definition for 'BOMData'
    
    Problem: CoreStubs.cs defined BOMData, BOMItem, UnfoldResult, etc.
    But BOMGenerator.cs and UnfoldEngine.cs ALSO define these classes in their #else blocks.
    This caused duplicate definition errors.
    
    Solution: Remove BOM and Unfold stub classes from CoreStubs.cs.
    These are already defined in their respective files:
    - BOMGenerator.cs has BOMData, BOMItem, BOMGeneratorStub in #else block
    - UnfoldEngine.cs has UnfoldResult, UnfoldData, BendInfo, UnfoldEngineStub in #else block
    
    CoreStubs.cs now only contains stubs for:
    - ChangePropagation (ChangePropagationEngineStub, AffectedPart, etc.)
    - Parametric (ParametricDrawingGeneratorStub, ParametricParameters, etc.)
    - SmartDimension (SmartDimensionEngineStub, DimensionConfig)
    
    File:
    - src/Core/CoreStubs.cs (removed duplicate BOM and Unfold classes)

--- src/Core/CoreStubs.cs
+++ src/Core/CoreStubs.cs
@@ -1,35 +1,8 @@
 // Core 项目云编译存根 - 提供公共类的空定义
 // 仅在 CLOUD_BUILD 模式下编译
+// 注意：BOM 和 Unfold 的存根类已在各自文件中定义，这里只包含其他模块的存根
 
 #if CLOUD_BUILD
-
-namespace CadAutomationPlugin.Core.BOM
-{
-    public class BOMGeneratorStub { }
-    
-    public class BOMData
-    {
-        public System.Collections.Generic.List<BOMItem> Items { get; set; } = new System.Collections.Generic.List<BOMItem>();
-        public string ProjectName { get; set; } = "";
-        public string DrawingNumber { get; set; } = "";
-        public System.DateTime GeneratedAt { get; set; } = System.DateTime.Now;
-        public string GeneratedBy { get; set; } = "";
-    }
-
-    public class BOMItem
-    {
-        public int Index { get; set; }
-        public string PartNumber { get; set; } = "";
-        public string Name { get; set; } = "";
-        public int Quantity { get; set; } = 1;
-        public string Material { get; set; } = "";
-        public double UnitWeight { get; set; }
-        public double TotalWeight { get; set; }
-        public string Vendor { get; set; } = "";
-        public string Description { get; set; } = "";
-        public string Remarks { get; set; } = "";
-    }
-}
 
 namespace CadAutomationPlugin.Core.ChangePropagation
 {
@@ -53,34 +26,6 @@ namespace CadAutomationPlugin.Core.SmartDimension
 {
     public class SmartDimensionEngineStub { }
     public class DimensionConfig { }
-}
-
-namespace CadAutomationPlugin.Core.Unfold
-{
-    public class UnfoldEngineStub { }
-    public class UnfoldResult 
-    { 
-        public string PartName { get; set; } = ""; 
-        public double FlatLength { get; set; } 
-        public int BendCount { get; set; } 
-        public double MaterialUtilization { get; set; } 
-        public string DrawingPath { get; set; } = ""; 
-    }
-    public class UnfoldData 
-    { 
-        public double FlatLength { get; set; } 
-        public int BendCount { get; set; } 
-        public double Thickness { get; set; } 
-        public double MaterialUtilization { get; set; } 
-        public System.Collections.Generic.List<BendInfo> BendData { get; set; } = new System.Collections.Generic.List<BendInfo>(); 
-    }
-    public class BendInfo 
-    { 
-        public double Position { get; set; } 
-        public double Angle { get; set; } 
-        public double Radius { get; set; } 
-        public double FlatLength => Radius * Angle * Math.PI / 180.0; 
-    }
 }
 
 #endif
